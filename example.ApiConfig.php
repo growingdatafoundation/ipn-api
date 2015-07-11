70e0d2fd8627e6a82208ac244f301ec82c68b7a7
Joerg Boeselt <net@boeselt.net>
2015-07-11 09:34:31 +0930

    apiconfig example

---
+++ example.ApiConfig.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Store sensible config data here and require this file in root file
+ */
+ 
+class ApiConfig{
+    
+    /**
+     * @var array Flickr API keys
+     */
+    public static $Flickr = array(
+        'key'       => '',
+        'secret'    => '',
+        'name'      => '',
+    );
+
+}
